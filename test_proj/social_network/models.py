815cc203e2eb2437a6294c632bdcf7e025334f66
viking <viking>
2022-02-16 17:50:39 +0200

    new

--- test_proj/social_network/models.py
+++ test_proj/social_network/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.hashers import make_password
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth.models import AbstractUser
 from django.utils.timezone import now
 
 
--- test_proj/test_proj/settings.py
+++ test_proj/test_proj/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'social_network.middleware.UpdateLastActivityMiddleware',
 ]
 
 ROOT_URLCONF = 'test_proj.urls'
